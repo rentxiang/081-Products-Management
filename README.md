0b4e8fd552e14a8705e518aa1715690e2af4ebcd
Tianxiang Ren <ren.tianx@northeastern.edu>
2023-02-14 14:40:45 -0800

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a products management system web app by using python and flask in the backend, mysql for the database.
---
+++ backend/sql_connection.py
@@ -0,0 +1,13 @@
+import datetime
+import mysql.connector
+
+__cnx = None
+
+def get_sql_connection():
+  print("Opening mysql connection")
+  global __cnx
+
+  if __cnx is None:
+    __cnx = mysql.connector.connect(user='root', password='rtx3388250', database='081_products')
+
+  return __cnx
